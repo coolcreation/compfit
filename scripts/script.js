b26405aef2cf6de12bb939fc6f05f13c19336c92
coolcreation <73644110+coolcreation@users.noreply.github.com>
2020-11-30 15:35:23 -0700

    new stuff
    new stuff

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+ 
+ /*  
+ Student name: Jeff Bohn
+ File name: script.js
+ Date: 11/30/2020
+
+ 
+$(document).ready(function(){
+    Add jQuery methods here   
+});   */
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
